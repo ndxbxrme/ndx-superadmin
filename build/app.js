2d152fb55797a631fd94ad3853936c540b66fb18
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-09 13:34:14 +0000

    permissions

--- build/app.js
+++ build/app.js
@@ -2,29 +2,29 @@
   'use strict';
   module.exports = function(ndx) {
     return ndx.database.on('ready', function() {
-      var users;
-      users = ndx.database.select(ndx.settings.USER_TABLE, {
+      return ndx.database.select(ndx.settings.USER_TABLE, {
         local: {
           email: 'superadmin@admin.com'
         }
-      });
-      if (!users.length) {
-        ndx.database.insert(ndx.settings.USER_TABLE, {
-          local: {
-            email: 'superadmin@admin.com',
-            password: ndx.generateHash('admin')
-          },
-          roles: {
-            superadmin: {}
-          }
-        });
-        console.log('superuser inserted');
-        return console.log('please remember to change the superadmin password');
-      } else {
-        if (ndx.validPassword('admin', users[0].local.password)) {
+      }, function(users) {
+        if (!users.length) {
+          ndx.database.insert(ndx.settings.USER_TABLE, {
+            local: {
+              email: 'superadmin@admin.com',
+              password: ndx.generateHash('admin')
+            },
+            roles: {
+              superadmin: {}
+            }
+          }, null, true);
+          console.log('superuser inserted');
           return console.log('please remember to change the superadmin password');
+        } else {
+          if (ndx.validPassword('admin', users[0].local.password)) {
+            return console.log('please remember to change the superadmin password');
+          }
         }
-      }
+      });
     });
   };
 
--- src/app.coffee
+++ src/app.coffee
@@ -2,19 +2,21 @@
 
 module.exports = (ndx) ->
   ndx.database.on 'ready', ->
-    users = ndx.database.select ndx.settings.USER_TABLE,
+    ndx.database.select ndx.settings.USER_TABLE,
       local:
         email: 'superadmin@admin.com'
-    if not users.length
-      ndx.database.insert ndx.settings.USER_TABLE,
-        local:
-          email: 'superadmin@admin.com'
-          password: ndx.generateHash 'admin'
-        roles:
-          superadmin: {}
-      console.log 'superuser inserted'
-      console.log 'please remember to change the superadmin password'
-    else
-      if ndx.validPassword 'admin', users[0].local.password
+    , (users) ->
+      if not users.length
+        ndx.database.insert ndx.settings.USER_TABLE,
+          local:
+            email: 'superadmin@admin.com'
+            password: ndx.generateHash 'admin'
+          roles:
+            superadmin: {}
+        , null, true
+        console.log 'superuser inserted'
         console.log 'please remember to change the superadmin password'
+      else
+        if ndx.validPassword 'admin', users[0].local.password
+          console.log 'please remember to change the superadmin password'
       
